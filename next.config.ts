2e664f771263893294a20f7585d5694d50529ceb
Denis Ruparel <deniskalpeshbhai436@gmail.com>
2025-03-01 11:07:08 +0530

    Fix next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true, // Ignores TypeScript errors when building
   },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignores ESLint errors during the build
+  },
+  env: {
+    DATABASE_ID: process.env.DATABASE_ID,
+    APPWRITE_PROJECT_ID: process.env.PROJECT_ID,
+    APPWRITE_API_KEY: process.env.API_KEY,
+  },
+  experimental: {
+  },
 };
 
 export default nextConfig;
